74df582f4ef4fb37be9b58a2f9af0b610be44980
Perry Gati <selasieperry@gmail.com>
2025-08-01 21:26:52 +0000

    view-app updates

--- app/Models/PermitRegistration.php
+++ app/Models/PermitRegistration.php
@@ -23,5 +23,17 @@ class PermitRegistration extends Model
      public function getDistrict(){
         return $this->belongsTo(District::class, 'district', 'id');
     }
+
+    public function type(){
+        return $this->belongsTo(ProjectType::class, 'type_id', 'id');
+    }
+
+    public function category(){
+        return $this->belongsTo(ProjectCategory::class, 'cat_id', 'id');
+    }
+
+    public function sector(){
+        return $this->belongsTo(ProjectSector::class, 'sector_id', 'id');
+    }
  
 }
